8defe181815101e3bdd5b52c3528a875922cb3a3
pengd-ksu <pengd@ksu.edu>
2024-09-24 23:21:53 -0700

    Finished chapter 5;

---
+++ ch5/RuntimeError.java
@@ -0,0 +1,10 @@
+package peng.ch5;
+
+class RuntimeError extends RuntimeException {
+  final Token token;
+
+  RuntimeError(Token token, String message) {
+    super(message);
+    this.token = token;
+  }
+}
